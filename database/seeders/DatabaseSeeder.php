13595eb1a27fdb270ad70d0680188946ad2057ba
Jean Pier <juanpedrormm92@gmail.com>
2021-03-29 22:58:56 -0300

    Blog init

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Curso;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         \App\Models\User::factory(10)->create();
+
+        Curso::factory(50)->create();
+    }
+}
